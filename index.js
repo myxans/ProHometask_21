7a7ae42066ac3c54439348b078e2c4f8d835324f
myxans <mushenko1992@gmail.com>
2023-02-21 23:50:08 +0200

    add_remote

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-function myFunc(array, callback) { 
-    let newArray = [];
-    callback(newArray, array);
-}
+const array = [
+    { name: "Jojo", age: 50 },
+    { name: "Boji", age: 35 },
+    { name: "Lufy", age: 18 }
+];
 
-function out(elem, arr) {
-    for (let i = 0; i < arr.length; i++) {
-        elem.push(arr[i] * 2)
-    }
-    console.log(elem);
-}
+function mapping (array, callback) {
+    let new_array = [];
+    array.forEach(function (element, index, array) {
+       new_array.push(callback(element)); 
+    });
+    return new_array;
+};
 
+const callback = (item, index, array) => item.age;
+const callback2 = (item, index, array) => item.age > 36;
 
-myFunc([5, 3, 2], out);
+console.log(mapping(array, callback));
+console.log(mapping(array, callback2));
+console.log(array);
