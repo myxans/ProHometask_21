fed7592d04310605f22e21aa2e38cf9388fd71e4
myxans <mushenko1992@gmail.com>
2023-02-23 19:06:52 +0200

    add_remote_3

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const array = [
     { name: "Lufy", age: 18 }
 ];
 
-function mapping (array, callback) {
+function mapping(array, callback) {
+    let new_array = [];
+    for (let key of array) {
+        new_array.push(callback(key))
+    }
+    return new_array;
+}
+
+
+
+
+/*function mapping (array, callback) {
     let new_array = [];
     array.forEach(function (element, index, array) {
        new_array.push(callback(element)); 
     });
     return new_array;
-};
+};*/
 
-const callback = (item, index, array) => item.age;
-const callback2 = (item, index, array) => item.age > 36;
+const callback = (item,) => item.age;
+const callback2 = (item) => item.age > 36;
 
 console.log(mapping(array, callback));
 console.log(mapping(array, callback2));
